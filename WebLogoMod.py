b0935b8a6a970b1b52cf296ba561d1a88612cbcc
kearseya <44373764+kearseya@users.noreply.github.com>
2019-09-25 20:19:58 +0100

    Update WebLogoMod.py
    Added multiple motif generation and displays the percentage of sequences used to make logo

--- WebLogoMod.py
+++ WebLogoMod.py
@@ -10,12 +10,16 @@ import numpy as np
 import math
 #import operator
 
-from KmerKounter import hamlist
-from KmerKounter import pwm
+#from KmerKounter import hamlist
+#from KmerKounter import pwm
 from KmerKounter import numofruns
 from KmerKounter import mink
 from KmerKounter import maxk
 from KmerKounter import startround
+
+from KmerKounter import top6all
+from KmerKounter import kmercount
+from KmerKounter import totaldict
 
 from KmerKounter import inputlist
 
@@ -26,75 +30,233 @@ from KmerKounter import mink
 from KmerKounter import maxk
 """
 
+def kmer2hash(kmer):
+    """
+    kmer: input sequence
+    return: a hash code, 32 bit
+    """
+    k = len(kmer)
+    assert k<16, "kmer should be shorted than 16 bases"
+    base = {'A': 0, 'C': 1, 'G': 2, 'T': 3, 'a': 0, 'c': 1, 'g': 2, 't': 3}
+    kh = np.zeros(1,dtype='uint32')
+    kh = base[kmer[0]]
+    for tb in kmer[1:]:
+        kh = kh<<2
+        kh += base[tb]
+    return kh
+
+def hash2kmer(hashkey, k):
+    """
+    hashkey: hash key of kmer, numpy, 'uint32'
+    k: length of kmer
+    """
+    base = np.array('ACGT', 'c')
+    arr = np.chararray(k)
+    mask = 0b11
+    arr[-1]=base[ mask & hashkey]
+    for i in range(2,k+1):
+        hashkey = (hashkey>>2)
+        arr[-i]=base[mask & hashkey]
+    return arr.tostring().decode("utf-8")
+
+
+
 def inputtype():
+    global nmotifs
     global type
     try:
+        from GUI import nmotifs
         from GUI import logotype
-        if len(logotype) > 1:
-            type = logotype[0]
+        #if len(logotype) > 1:
+        type = logotype[0]
     except:
-        type = input("Bits (b) or Frequency (f)?:")
+        nmotifs = int(input("Number of motifs: "))
+        type = input("Bits (b) or Frequency (f)?: ")
 
 inputtype()
 
 #print("PWM")
 #print(pwm)
 
-def En(n):
-    Enn = ((1/math.log(2))*((4-1)/(2*n)))
+hamlist = []
+removelist = []
+hamdict = []
+countdict = []
+pwm = []
+logoform = []
+
+def dictinit(numofruns):
+    for _ in range(numofruns+1):
+        #kmercount.append({})
+        hamlist.append({})
+        removelist.append({})
+        hamdict.append({})
+        countdict.append({})
+        pwm.append({})
+        logoform.append({})
+
+dictinit(numofruns)
+
+
+def hamming_distance(s1, s2):
+    if len(s1) != len(s2):
+        raise ValueError("Undefined for sequences of unequal length")
+    return sum(ch1 != ch2 for ch1, ch2 in zip(s1, s2))
+
+
+
+def listhammer():
+    for runnum in range(1, numofruns+1):
+        for i in kmercount[runnum]:
+            hamlist[runnum][i] = {}
+            removelist[runnum][i] = set()
+            for n in range(1, nmotifs+1):
+                hamlist[runnum][i][n] = []
+                #hconsensus = (list(kmercount[runnum][i].keys())[0])
+                if n == 1:
+                    hconsensus = max(kmercount[runnum][i], key=lambda key: kmercount[runnum][i][key])
+                else:
+                    po = 0
+                    for p, x in enumerate(top6all[runnum][i], 1):
+                        if p > po:
+                            if x not in removelist[runnum][i]:
+                                hconsensus = x
+                                po = p
+                consensus = hash2kmer(hconsensus, i)
+                for x in list(kmercount[runnum][i].keys()):
+                    values = hash2kmer(x,i)
+                    if hamming_distance(consensus, values) <= 1:
+                        if x not in removelist[runnum][i]:
+                            hamlist[runnum][i][n].append(x)
+                            removelist[runnum][i].add(x)
+
+
+
+listhammer()
+
+#print(hamlist)
+
+def dicthammer():
+    global countdict
+    for runnum in range(1, numofruns+1):
+        for i in hamlist[runnum]:
+            hamdict[runnum][i] = {}
+            countdict[runnum][i] = {}
+            for n in range(1, nmotifs+1):
+                hamdict[runnum][i][n] = {}
+                countdict[runnum][i][n] = 0
+                test = { z : kmercount[runnum][i][z] for z in hamlist[runnum][i][n] }
+                #M = sum(kmercount[runnum][i].values())
+                M = sum(test.values())
+                test = { z : (kmercount[runnum][i][z]/M) for z in test }
+                hamdict[runnum][i][n].update(test)
+                for x in hamlist[runnum][i][n]:
+                    countdict[runnum][i][n] += kmercount[runnum][i][x]
+
+dicthammer()
+
+#print("Count")
+#print(countdict)
+
+def startpwm():
+    for runnum in range(1, numofruns+1):
+        for i in hamdict[runnum]:
+            pwm[runnum][i] = {}
+            logoform[runnum][i] = {}
+            for n in range(1, nmotifs+1):
+                pwm[runnum][i][n] = {}
+                logoform[runnum][i][n] = []
+                for j in range(1,i+1):
+                    pwm[runnum][i][n][j] = {"A":0}
+                    pwm[runnum][i][n][j].update({"C":0})
+                    pwm[runnum][i][n][j].update({"G":0})
+                    pwm[runnum][i][n][j].update({"T":0})
+
+startpwm()
+
+def pwmmaker():
+    for runnum in range(1, numofruns+1):
+        for i in hamdict[runnum]:
+            for n in range(1, nmotifs+1):
+                for x in hamdict[runnum][i][n]:
+                    kmer = hash2kmer((x),i)
+                    for j in range(1,i+1):
+                        if kmer[j-1] == "A":
+                            pwm[runnum][i][n][j]["A"] += hamdict[runnum][i][n][x]
+                        elif kmer[j-1] == "C":
+                            pwm[runnum][i][n][j]["C"] += hamdict[runnum][i][n][x]
+                        elif kmer[j-1] == "T":
+                            pwm[runnum][i][n][j]["T"] += hamdict[runnum][i][n][x]
+                        elif kmer[j-1] == "G":
+                            pwm[runnum][i][n][j]["G"] += hamdict[runnum][i][n][x]
+
+pwmmaker()
+
+
+
+def En(num):
+    Enn = ((1/math.log(2))*((4-1)/(2*num)))
     return Enn
 
-def Shannon(runnum, k, pos):
+def Shannon(runnum, k, pos, n):
     bases = ["A", "T", "C", "G"]
     Hi = 0
     for b in bases:
-        f = pwm[runnum][k][pos][b]
+        f = pwm[runnum][k][n][pos][b]
         try:
             Hi += -(f*math.log2(f))
         except:
             continue
     return Hi
 
-def Ri(runnum, k, i):
-    n = len(hamlist[runnum][k])
-    Rii = math.copysign((math.log2(4)-(Shannon(runnum, k, i) + En(n))), 1)
+def Ri(runnum, k, i, n):
+    num = len(hamlist[runnum][k][n])
+    Rii = math.copysign((math.log2(4)-(Shannon(runnum, k, i, n) + En(num))), 1)
     return Rii
 
 
-logoform = []
 
+"""
 def dictinit():
-    for r in range(0, numofruns):
+    for r in range(0, numofruns+1):
         logoform.append({})
         for k in range(mink, maxk+1):
-            logoform[r].update({k:[]})
+            logoform[r][k] = {}
+            #logoform[r].update({k:[]})
+            for n in range(1, nmotifs+1):
+                logoform[r][k][n] = []
 dictinit()
-
+"""
 
 def logopos(a,t,c,g):
     upos =  [('A', a), ('T', t), ('C', c), ('G', g)]
     pos = sorted(upos, key=lambda x:x[1])
     return pos
 
+#print("PWM")
+#print(pwm)
+#print("LOGO")
+#print(logoform)
 
-def kmerpwm(runnum, k):
+
+def kmerpwm(runnum, k, n):
     kmerpwm = []
     for i in range(1,k+1):
         if type == "f":
-            kmerpwm.append(logopos(pwm[runnum][k][i]["A"],pwm[runnum][k][i]["T"],pwm[runnum][k][i]["C"],pwm[runnum][k][i]["G"]))
+            kmerpwm.append(logopos(pwm[runnum][k][n][i]["A"],pwm[runnum][k][n][i]["T"],pwm[runnum][k][n][i]["C"],pwm[runnum][k][n][i]["G"]))
         if type == "b":
-            kmerpwm.append(logopos(pwm[runnum][k][i]["A"]*Ri(runnum, k, i),pwm[runnum][k][i]["T"]*Ri(runnum, k, i),pwm[runnum][k][i]["C"]*Ri(runnum, k, i),pwm[runnum][k][i]["G"]*Ri(runnum, k, i)))
+            kmerpwm.append(logopos(pwm[runnum][k][n][i]["A"]*Ri(runnum, k, i, n),pwm[runnum][k][n][i]["T"]*Ri(runnum, k, i, n),pwm[runnum][k][n][i]["C"]*Ri(runnum, k, i, n),pwm[runnum][k][n][i]["G"]*Ri(runnum, k, i, n)))
     return kmerpwm
 
 
 
 def allmaker(numofruns):
-    for z in range(startround, (numofruns+startround)):
+    for z in range(1, numofruns+1):
         #mink = int(inputlist[((z-1)*5)+7])
         #maxk = int(inputlist[((z-1)*5)+8])
         for j in range(mink,maxk+1):
-            #print(kmerpwm(z,j))
-            logoform[z-startround][j].append(kmerpwm(z,j))
+            for n in range(1, (nmotifs+1)):
+                logoform[z][j][n].append(kmerpwm(z,j,n))
 
 allmaker(numofruns)
 
@@ -119,11 +281,11 @@ class Scale(matplotlib.patheffects.RendererBase):
         affine = affine.identity().scale(self._sx, self._sy)+affine
         renderer.draw_path(gc, tpath, affine, rgbFace)
 
-def draw_logo(all_scores, run, k):
+def draw_logo(all_scores, run, k, n):
     fig = plt.figure()
-    fig.set_size_inches(k+(k*0.2), 2)
+    fig.set_size_inches(k, 2)
     ax = fig.add_subplot(111)
-    ax.set_xticks(range(k))
+    #ax.set_xticks(range(k))
 
     xshift = 0
     trans_offset = transforms.offset_copy(ax.transAxes,
@@ -161,21 +323,26 @@ def draw_logo(all_scores, run, k):
         ax.set_ylabel("frequency")
     if type == "b":
         ax.set_ylabel("bits")
-
-    seaborn.despine(ax=ax, offset=30, trim=True)
-    ax.set_xticklabels(range(1,len(all_scores)+1), rotation=90)
+    ax.set_title("Uses "+str(round((countdict[run][k][n]/totaldict[run][k])*100, 2))+"%")
+    #ax.spines['bottom'].set_visible(False)
+    #ax.spines['top'].set_visible(False)
+    #ax.spines['right'].set_visible(False)
+    plt.tick_params(bottom = False, labelbottom = False)
+    seaborn.despine(ax=ax, offset=30, trim=False, bottom=True)
+    #ax.set_xticklabels(range(1,len(all_scores)+1), rotation=90)
     ax.set_yticklabels(np.arange(0,3,1))
     #plt.show()
-    plt.savefig('figures/logo_'+str(identifier)+"_"+str(run)+"_"+str(k), dpi=600)
+    plt.savefig("figures/"+str(identifier)+"/logos/logo_"+str(identifier)+"_"+str(run+(startround-1))+"_"+str(k)+"_"+str(n), dpi=600, bbox_inches='tight')
     plt.close()
 
 
 
 def logoprinter():
-    for r in range(startround, numofruns+startround):
-        for i in logoform[r-1]:
-            for j in range(0, len(logoform[r-1][i])):
-                draw_logo(logoform[r-1][i][j], r, i)
+    for r in range(1, numofruns+1):
+        for i in logoform[r]:
+            for n in range(1, nmotifs+1):
+                for j in range(0, len(logoform[r][i][n])):
+                    draw_logo(logoform[r][i][n][j], r, i, n)
 
 
 logoprinter()
