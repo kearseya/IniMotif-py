c8d0587d03f22ccba01f45a1ae36d0e717709de9
kearseya <44373764+kearseya@users.noreply.github.com>
2019-11-03 16:06:24 +0000

    Create masker.py

---
+++ masker.py
@@ -0,0 +1,75 @@
+import re
+#from Bio import SeqIO
+import os
+
+from itertools import chain, combinations, product
+
+revnuc = {'A':'T','T':'A','G':'C','C':'G','N':'N'}
+
+def revComp(seq):
+    rev = ''
+    for i in range(len(seq) - 1,-1,-1):
+        rev += revnuc[seq[i]]
+    return rev
+
+def hamming_circle(s, n, alphabet):
+    for positions in combinations(range(len(s)), n):
+        for replacements in product(range(len(alphabet) - 1), repeat=n):
+            cousin = list(s)
+            for p, r in zip(positions, replacements):
+                if cousin[p] == alphabet[r]:
+                    cousin[p] = alphabet[-1]
+                else:
+                    cousin[p] = alphabet[r]
+            yield ''.join(cousin)
+
+def hamming_ball(s, n, alphabet):
+    allowedkmers = set(chain.from_iterable(hamming_circle(s, i, alphabet) for i in range(n + 1)))
+    return allowedkmers
+
+def myrepl(m):
+    return ('N'*len(m.group(0)))
+
+#pat = re.compile(f'({unit_str}){{{n_min_unit},{n_max_unit}}}')
+#out_str = re.sub(pat, myrepl, instr)
+max_rep = ''
+
+file = str(input("File to be masked (with path): "))
+
+def masker():
+    infile = open(file, "r")
+    outfile = open(str(os.path.dirname(file))+"/masked_"+str(os.path.basename(file)), "w")
+    numberofmasks = int(input("Number of masks: "))
+    for n in range(1, numberofmasks+1):
+        typeofmask = str(input("Mask type (repeat/motif): "))
+        if typeofmask in ["repeat", "r", "rep"]:
+            unit = str(input("Unit string: "))
+            min_rep = int(input("Minimum repeats: "))
+            pat = re.compile(f'({unit}){{{min_rep},{max_rep}}}')
+            for line in infile:
+                out_str = re.sub(pat, myrepl, line)
+                outfile.write(out_str)
+        if typeofmask in ["motif", "m", "mot"]:
+            unit = str(input("Unit string: "))
+            min_rep = 1
+            revwant = str(input("Mask revcomp of unit?: "))
+            allowham = int(input("Hamming distance: "))
+            maskmotifs = hamming_ball(unit, allowham, "ATGC")
+            rmaskmotifs = set()
+            if revwant in ["y", "Y", "yes", "Yes", "t", "true", "True"]:
+                for i in maskmotifs:
+                    rmaskmotifs.add(i)
+                    rmaskmotifs.add(revComp(i))
+                for kmer in rmaskmotifs:
+                    pat = re.compile(f'({kmer}){{{min_rep},{max_rep}}}')
+                    for line in infile:
+                        out_str = re.sub(pat, myrepl, line)
+                        outfile.write(out_str)
+            else:
+                for kmer in maskmotifs:
+                    pat = re.compile(f'({kmer}){{{min_rep},{max_rep}}}')
+                    for line in infile:
+                        out_str = re.sub(pat, myrepl, line)
+                        outfile.write(out_str)
+
+masker()
